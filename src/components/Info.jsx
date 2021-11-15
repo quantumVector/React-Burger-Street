30bef72b91012980d23e0ffb9a0b542c7774a8de
quantumVector <payned@mail.ru>
2021-11-15 12:11:45 +0300

    added info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import classes from '../styles/Info.module.css';
+import clock from '../assets/clock.png';
+import phone from '../assets/phone.png';
 
 const Info = () => {
   return (
-    <div>
-
+    <div className={classes.info}>
+      <div className={classes.item}>
+        <img className={classes.icon} src={clock} alt="icon" />
+        <div className={classes.wrap}>
+          <div className={classes.title}>Время работы:</div>
+          <div className={classes.hours}>с 10:00 до 23:00</div>
+        </div>
+      </div>
+      <div className={classes.item}>
+        <img className={classes.icon} src={phone} alt="icon" />
+        <div className={classes.wrap}>
+          <div className={classes.phone}>+7 123 456 78 90</div>
+          <a className={classes.contact} href="tel:+7 (1234) 56 78 90">Позвоните нам</a>
+        </div>
+      </div>
     </div>
   )
 }
